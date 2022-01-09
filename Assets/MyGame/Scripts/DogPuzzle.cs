951e7aaba172c2c6c21ae0c163712f5961e30e45
pklacher <pia.lacher04@htl-salzburg.ac.at>
2022-01-09 16:12:58 +0100

    Fix Dog Game

--- Assets/MyGame/Scripts/DogPuzzle.cs
+++ Assets/MyGame/Scripts/DogPuzzle.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DogPuzzle : MonoBehaviour
 {
-    public GameObject handy;
+    /*public GameObject handy;
     public NumerInput numberScript;
+
+    //public TMP_Text congratsText;
 
     // Start is called before the first frame update
     void Start()
     {
         handy.SetActive(false);
-        
+        congratsText.text = "";
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log(other.name);
 
-        if (other.name == "LeftHandController" || other.name == "RightHandBaseController")
+        /*if (other.name == "LeftHandController" || other.name == "RightHandBaseController")
         {
             Debug.Log("trigger handy");
             handy.SetActive(true);
@@ -37,18 +40,27 @@ public class DogPuzzle : MonoBehaviour
         }
     }
 
-    private void rightNumber(GameObject noHandy)
+    private void rightNumber()
     {
         if(numberScript.telephoneNumberInput == numberScript.telephoneNumberRight)
         {
-            //noHandy = handy.(DestroyObject);
+            //handy
+            Congrats();
         }
     }
+
+
+    private void Congrats()
+    {
+     congratsText.text = "Du hast es geschafft!\nGehe weiter und löse auch die restlichen Puzzles!\n(1/3)";
+    }
+
+    
 
 
       // Update is called once per frame
     void Update()
     {
     
-    }
+    }*/
 }
--- Assets/MyGame/Scripts/NumberInput.cs
+++ Assets/MyGame/Scripts/NumberInput.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class NumerInput : MonoBehaviour
+public class NumberInput : MonoBehaviour
 {
     public GameObject handy;
     public GameObject dog;
 
-    //public DogPuzzle dopPuzzleScript;
 
     public string telephoneNumberRight = "06";
     public string telephoneNumberInput;
 
+    public TMP_Text congratsText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        handy.SetActive(false);
+        congratsText.text = "";
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "number")
+        if (other.name == "Handy_Trigger")
+        {
+            Debug.Log("trigger handy");
+            handy.SetActive(true);
+        }
+
+        if (other.tag == "number")
         {
             Debug.Log("input number");
             string numberIn = other.name;
@@ -30,9 +39,9 @@ public class NumerInput : MonoBehaviour
             if (telephoneNumberInput == telephoneNumberRight)
             {
                 Debug.Log("Congrats");
-                //dog.SetActive(false);
-
-
+                Congrats();
+                dog.SetActive(false);
+                Destroy(handy);
             }
         }
         if(other.tag == "delete")
@@ -41,6 +50,22 @@ public class NumerInput : MonoBehaviour
             Debug.Log("delete");
         }
         
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log(other.name);
+
+        if (other.name == "Handy_Trigger")
+        {
+            Debug.Log("trigger handy leave");
+            handy.SetActive(false);
+        }
+    }
+
+    private void Congrats()
+    {
+        congratsText.text = "Du hast es geschafft!\nGehe weiter und löse auch die restlichen Puzzles!\n(1/3)";
     }
 
     // Update is called once per frame
